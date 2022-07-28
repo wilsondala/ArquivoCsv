da2302d59aaba314469360796499c00019c9946e
Wilson Dala <99853733+wilsondala@users.noreply.github.com>
2022-07-28 18:14:21 -0300

    conclusaoDos_desafios-2e3

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSVReader
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string linha = "nome, cargo, cidade, telefone, endereco";
+            string[] linhaseparada = null;
+            StreamReader reader = new StreamReader(@"C:\arquivoCsv\pessoas.csv", Encoding.UTF8, true);
+            while (true)
+            {
+                linha = reader.ReadLine();
+                if (linha == null) break;
+                linhaseparada = linha.Split(';');                
+                string resultado = $@"Linha - S 
+                    Nome : {(object)linhaseparada[0]}
+                    Cargo: {(object)linhaseparada[1]}
+                    Cidade: {(object)linhaseparada[2]}
+                    Telefone: {(object)linhaseparada[3]}
+                    Endereço: {(object)linhaseparada[4]}";
+                Console.WriteLine(resultado);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ArquivoCsv
 Arquivo Csv feito no excel exportado no no console do C#
+
+Olá eu sou o wilson este desafio tem  por objetivo de testar as minhas capacidade tecnicas.
+
+foi desafiado a criar um arquivo CSV no Excel, e esportar no console do C# Sharp. 
+
+feito no no editor  de texto Vscode. 
+Para o desafio foi preciso usar o terminal, onde  por meio do  node foi capaz de criar o  projeto com os seguite comando:
+
+1 - mkdir - para criar a pasta do projeto
+2 - dotnet new console --framework net6.0
